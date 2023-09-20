f3e390de85e12d943fc567d96dc0f04722e4347a
Trixie_LM <octavia-94@bk.ru>
2023-09-20 10:13:13 +0300

    Исправление дефектов

--- test_search.py
+++ test_search.py
@@ -2,11 +2,11 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_search(open_browser):
+def test_search():
     browser.element('#APjFqb').should(be.blank).type('yashaka/selene').press_enter()
-    assert browser.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
+    browser.element('#search').should(have.text('yashaka/selene: User-oriented Web UI browser tests in ...'))
 
 
-def test_negative_search(open_browser):
+def test_negative_search():
     browser.element('#APjFqb').should(be.blank).type('EXRDCFTVYGBHUnjimko,lpkmjnuhbgyft').press_enter()
-    assert browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
+    browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
