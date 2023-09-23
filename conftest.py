bc7a7c0883919dd962e20b1b8565684ae341c27b
Trixie_LM <octavia-94@bk.ru>
2023-09-23 22:17:03 +0300

    conflict

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selene.support.shared import browser
 
 @pytest.fixture(autouse=True)
 def open_browser():
-    browser.config.window_width = 1600
+    browser.config.window_width = 1500
     browser.config.window_height = 1200
     browser.open('https://google.com')
     yield
