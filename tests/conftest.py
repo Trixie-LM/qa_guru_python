2ad1b68f7092afdbe15d9ced2da77da990b97e8d
Trixie_LM <octavia-94@bk.ru>
2023-10-02 19:58:10 +0300

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,22 @@
 import pytest
 from selene.support.shared import browser
+from selene import be, have, command
+from selene.support import by
 
 
-@pytest.fixture(autouse=True)
-def open_browser():
+@pytest.fixture(scope='function', autouse=True)
+def browser_management():
+
+    # Browser settings
     browser.config.browser_name = 'chrome'
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1900
     browser.config.window_height = 1600
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.config.timeout = 6.0
+
+    # Browser operations
+    browser.open('/automation-practice-form')
+    browser.should(have.title_containing('DEMOQA'))
     yield
     browser.quit()
 
---
+++ tests/test_registration_form.py
@@ -0,0 +1,53 @@
+from selene.support.shared import browser
+from selene import be, have, command
+
+
+def test_registration_form():
+
+    # firstName
+    browser.element('#firstName').should(be.blank).type('Pinkamena')
+    # lastName
+    browser.element('#lastName').should(be.blank).type('Pie')
+    # Email
+    browser.element('#userEmail').should(be.blank).type('Cakes_and_bakery@mail.com')
+
+    # Gender
+    browser.element("label[for='gender-radio-2']").click()
+
+    # Phone
+    browser.element('#userNumber').should(be.blank).type('9991234567')
+
+    # Date of Birth
+    browser.element('#dateOfBirthInput').click()
+    browser.element("[class='react-datepicker__day react-datepicker__day--023']").click()
+
+    # Subjects
+    # browser.element('#dateOfBirthInput').should(be.blank).click()
+
+    # Hobbies
+    # browser.element('#hobbies-checkbox-1').click()
+
+    # Picture
+
+    # Current Address
+    browser.element('#currentAddress').should(be.blank).type('Sugar Palace, a candy store in Ponyville.')
+
+    # State
+    browser.element('.css-19bqh2r').click()
+    browser.element('#react-select-3-option-2').click()
+    # City
+    browser.element("div[class=' css-1wa3eu0-placeholder']").click()
+    browser.element('#react-select-4-option-1').click()
+
+    # Button
+    browser.element('#submit').click()
+
+    # Assertions
+    browser.all('tbody td:nth-of-type(2)').should(have.exact_texts(
+        'Pinkamena',
+        'Pie',
+        'Cakes_and_bakery@mail.com',
+        'Pinkamena',
+        'Pie',
+        'Cakes_and_bakery@mail.com'
+    ))
