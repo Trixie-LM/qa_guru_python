97df0c93ab470bd3aaa219893056932373a43f78
Trixie_LM <octavia-94@bk.ru>
2023-10-11 09:59:38 +0300

    Another one

--- test_homework.py
+++ test_homework.py
@@ -4,35 +4,39 @@ import openpyxl
 from PyPDF2 import PdfReader
 import pandas as pd
 
-def test_check_archived_txt():
+
+def test_txt_file():
     with ZipFile('tmp/testzip.zip') as zf:
         assert zf.read('txt_file.txt').decode('utf-8') == 'txt_file'
         assert zf.getinfo('txt_file.txt').file_size == os.path.getsize('resources/txt_file.txt')
 
-def test_check_archived_pdf():
-    with ZipFile('tmp/testzip.zip') as zf:
-        archived_pdf = PdfReader(zf.open('bar_menu.pdf'))
-        non_archived_pdf = PdfReader('resources/bar_menu.pdf')
-        assert len(archived_pdf.pages) == len(non_archived_pdf.pages)
-        assert 'Кугель  с грибным соусом' in archived_pdf.pages[0].extract_text()
-        assert zf.getinfo('bar_menu.pdf').file_size == os.path.getsize('resources/bar_menu.pdf')
 
-def test_check_archived_xls():
+def test_pdf_file():
     with ZipFile('tmp/testzip.zip') as zf:
-        archived_book_read = pd.read_excel(zf.open('Company_staff.xls'))
-        archived_book_excel = pd.ExcelFile(zf.open('Company_staff.xls'))
-        non_archived_book_read = pd.read_excel('resources/Company_staff.xls')
-        non_archived_book_excel = pd.ExcelFile('resources/Company_staff.xls')
+        archived_pdf = PdfReader(zf.open('pdf_file.pdf'))
+        pdf = PdfReader('resources/pdf_file.pdf')
+        assert len(archived_pdf.pages) == len(pdf.pages)
+        assert 'Пример PDF файла' in archived_pdf.pages[0].extract_text()
+        assert zf.getinfo('pdf_file.pdf').file_size == os.path.getsize('resources/pdf_file.pdf')
+
+
+def test_xls_file():
+    with ZipFile('tmp/testzip.zip') as zf:
+        archived_book_read = pd.read_excel(zf.open('xls_file.xls'))
+        archived_book_excel = pd.ExcelFile(zf.open('xls_file.xls'))
+        non_archived_book_read = pd.read_excel('resources/xls_file.xls')
+        non_archived_book_excel = pd.ExcelFile('resources/xls_file.xls')
         assert archived_book_read.columns.any() == non_archived_book_read.columns.any() and archived_book_read.columns.all() == non_archived_book_read.columns.all()
         assert archived_book_excel.sheet_names == non_archived_book_excel.sheet_names
         assert archived_book_read.shape[0] == non_archived_book_read.shape[0] and archived_book_read.shape[1] == non_archived_book_read.shape[1]
-        assert zf.getinfo('Company_staff.xls').file_size == os.path.getsize('resources/Company_staff.xls')
+        assert zf.getinfo('xls_file.xls').file_size == os.path.getsize('resources/xls_file.xls')
 
-def test_check_archived_xlsx():
+
+def test_xlsx_file():
     with ZipFile('tmp/testzip.zip') as zf:
-        archived_book = openpyxl.load_workbook(zf.open('Office_characters.xlsx')).active
-        non_archived_book = openpyxl.load_workbook('resources/Office_characters.xlsx').active
-        assert archived_book.cell(row=2, column=2).value == non_archived_book.cell(row=2, column=2).value
-        assert archived_book.max_column == non_archived_book.max_column
-        assert archived_book.max_row == non_archived_book.max_row
-        assert zf.getinfo('Office_characters.xlsx').file_size == os.path.getsize('resources/Office_characters.xlsx')
+        archived_book = openpyxl.load_workbook(zf.open('xlsx_file.xlsx')).active
+        book = openpyxl.load_workbook('resources/xlsx_file.xlsx').active
+        assert archived_book.cell(row=1, column=1).value == book.cell(row=1, column=1).value
+        assert archived_book.max_column == book.max_column
+        assert archived_book.max_row == book.max_row
+        assert zf.getinfo('xlsx_file.xlsx').file_size == os.path.getsize('resources/xlsx_file.xlsx')
