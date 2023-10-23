43fb90245c1b8e635c90fe55119c2f029e7b9e9f
Trixie_LM <octavia-94@bk.ru>
2023-10-23 20:31:11 +0300

    Preparing homework

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -1,88 +1,44 @@
 import os
-from selene import command, have, browser
+from selene import command, have, browser, be
 
 
 class RegistrationPage:
-    def __init__(self):
-        self.first_name = browser.element('#firstName')
-        self.last_name = browser.element('#lastName')
-        self.email = browser.element('#userEmail')
-        self.gender = browser.element('label[for="gender-radio-1')
-        self.user_mobile = browser.element('#userNumber')
-        self.date_of_birth_input = browser.element('#dateOfBirthInput')
-        self.date_of_birth_month = browser.element('.react-datepicker__month-select')
-        self.date_of_birth_year = browser.element('.react-datepicker__year-select')
-        self.subjects_input = browser.element('#subjectsInput')
-        self.hobbies = browser.element('//label[. ="Sports"]')
-        self.picture = browser.element('#uploadPicture')
-        self.address = browser.element('#currentAddress')
-        self.state = browser.element('#state')
-        self.state_select = browser.element('#react-select-3-input')
-        self.city = browser.element('#city')
-        self.city_select = browser.element('#react-select-4-input')
-        self.submit = browser.element('#submit')
-
     def open(self):
         browser.open('/automation-practice-form')
         browser.should(have.title_containing('DEMOQA'))
 
-    def type_first_name(self, value):
-        self.first_name.type(value)
+    def register_student(self, user):
+        browser.element('#firstName').should(be.visible).type(user.first_name)
+        browser.element('#lastName').should(be.visible).type(user.last_name)
+        browser.element('#userEmail').should(be.visible).type(user.email)
+        browser.all('.custom-radio').element_by(have.text(user.gender)).click()
+        browser.element('#userNumber').should(be.visible).type(user.mobile)
+        browser.element('#dateOfBirthInput').should(be.visible).click()
+        browser.element('.react-datepicker__year-select').type(user.yearofbirth)
+        browser.element('.react-datepicker__month-select').type(user.mohtofberth)
+        browser.element(f'.react-datepicker__day--0{user.dayofbirth}').click()
+        browser.element('#subjectsInput').should(be.visible).type(user.subject).press_enter()
+        browser.all('.custom-checkbox').element_by(have.exact_text(user.hobbies)).click()
+        browser.element('#uploadPicture').should(be.visible).type(user.picture)
+        browser.element('#currentAddress').should(be.visible).type(user.address)
+        browser.element("#react-select-3-input").should(be.visible).type(user.state).press_enter()
+        browser.element("#react-select-4-input").should(be.visible).type(user.city).press_enter()
+        browser.element('#submit').perform(command.js.click)
 
-    def type_last_name(self, value):
-        self.last_name.type(value)
-
-    def type_email(self, value):
-        self.email.type(value)
-
-    # TODO: Сделать разветвление
-    def click_gender(self, value):
-        self.gender.click()
-
-    def type_number(self, value):
-        self.user_mobile.type(value)
-
-    # TODO: Проверить
-    def type_date_of_birth(self, day, month, year):
-        self.date_of_birth_input.click()
-        self.date_of_birth_month.type(month)
-        self.date_of_birth_year.type(year)
-        browser.element(
-            f'.react-datepicker__day--00{day}:not(.react-datepicker__day--outside-month)'
-        ).click()
-
-    def type_subjects(self, value):
-        self.subjects_input.type(value).press_enter()
-
-    # TODO: Сделать разветвление
-    def click_hobbies(self, value):
-        # browser.element('[for="hobbies-checkbox-1"]').click()
-        browser.all('.custom-checkbox').element_by(have.exact_text(value)).click()
-
-    def download_picture(self, file):
-        self.picture.send_keys(file)
-
-    def type_address(self, value):
-        self.address.type(value)
-
-    def type_state(self, value):
-        self.state.perform(command.js.scroll_into_view)
-        self.state.click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(value)).click()
-
-    def type_city(self, value):
-        browser.element('#city').click()
-        browser.all('[id^=react-select][id*=option]').element_by(
-            have.exact_text(value)).click()
-
-    def click_submit(self):
-        self.submit.click()
-
-    def registered_user_data_should(self, full_name, email, gender, number,
-                                    date, subject, hobbie, file, address, state_city):
+    def should_have_registered(self, user):
+        full_name = f'{user.first_name} {user.last_name}'
+        stateandcity = f'{user.state} {user.city}'
+        dateofbirth = f'{user.dayofbirth} {user.mohtofberth},{user.yearofbirth}'
         browser.element('.table').all('td').even.should(
-            have.exact_texts(
-                full_name, email, gender, number, date, subject, hobbie, file, address, state_city,
-            )
-        )
+            have.texts(
+                full_name,
+                user.email,
+                user.gender,
+                user.mobile,
+                dateofbirth,
+                user.subject,
+                user.hobbies,
+                user.picture,
+                user.address,
+                stateandcity
+            ))
--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,40 +1,28 @@
 import os
-
-from selene.support.shared import browser
-from selene import be, have
-
+from data.users import User
 from pages.registration_page import RegistrationPage
 
 
 registration_page = RegistrationPage()
 
-
-def test_registration_form():
-
-    registration_page.open()
-    registration_page.type_first_name('Pinkamena')
-    registration_page.type_last_name('Pie')
-    registration_page.type_email('Cakes_and_bakery@mail.com')
-    registration_page.click_gender('male')
-    registration_page.type_number('9991234567')
-    registration_page.type_date_of_birth('9', 'August', '2023')
-    registration_page.type_subjects('Maths')
-    registration_page.click_hobbies('Music')
-    registration_page.download_picture(os.path.abspath('image/Trixie.jpeg'))
-    registration_page.type_address('Sugar Palace, a candy store in Ponyville.')
-    registration_page.type_state('NCR')
-    registration_page.type_city('Noida')
-    registration_page.click_submit()
-
-    registration_page.registered_user_data_should(
-        'Pinkamena Pie',
-        'Cakes_and_bakery@mail.com',
-        'Male',
-        '9991234567',
-        '09 August,2023',
-        'Maths',
-        'Music',
-        'Trixie.jpeg',
-        'Sugar Palace, a candy store in Ponyville.',
-        'NCR Noida'
+def test_registration_student():
+    student = User(
+        first_name='Pinkamena',
+        last_name='Pie',
+        email='Cakes_and_bakery@mail.com',
+        gender='Female',
+        mobile='9991234567',
+        dayofbirth='09',
+        mohtofberth='August',
+        yearofbirth='2023',
+        subject='Maths',
+        hobbies='Music',
+        picture=os.path.abspath('image/Trixie.jpeg'),
+        address='Sugar Palace, a candy store in Ponyville.',
+        state='NCR',
+        city='Noida'
     )
+    registration_page.open()
+    registration_page.register_student(student)
+    registration_page.should_have_registered(student)
+
