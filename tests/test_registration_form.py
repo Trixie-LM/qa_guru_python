2fdb9738c255f250be8bd2e1f8c99d6c55ec1657
Trixie_LM <octavia-94@bk.ru>
2023-11-05 16:11:41 +0300

    COMMIT

--- tests/test_registration_form.py
+++ tests/test_registration_form.py
@@ -1,34 +1,39 @@
 from pages.registration_page import RegistrationPage
 from pathlib import Path
+import allure
 
 registration_page = RegistrationPage()
 
 
 def test_registration_form():
-    registration_page.open()
-    registration_page.type_first_name('Pinkamena')
-    registration_page.type_last_name('Pie')
-    registration_page.type_email('Cakes_and_bakery@mail.com')
-    registration_page.click_gender('male')
-    registration_page.type_number('9991234567')
-    registration_page.type_date_of_birth('23', 'August', '2023')
-    registration_page.type_subjects('Maths')
-    registration_page.click_hobbies('Music')
-    registration_page.download_picture(str(Path(__file__).parent.joinpath(f'image/Trixie.jpeg')))
-    registration_page.type_address('Sugar Palace, a candy store in Ponyville.')
-    registration_page.type_state('NCR')
-    registration_page.type_city('Noida')
-    registration_page.click_submit()
+    with allure.step('Open registrations form'):
+        registration_page.open()
 
-    registration_page.registered_user_data_should(
-        'Pinkamena Pie',
-        'Cakes_and_bakery@mail.com',
-        'Male',
-        '9991234567',
-        '23 August,2023',
-        'Maths',
-        'Music',
-        'Trixie.jpeg',
-        'Sugar Palace, a candy store in Ponyville.',
-        'NCR Noida'
-    )
+    with allure.step('Fill form'):
+        registration_page.type_first_name('Pinkamena')
+        registration_page.type_last_name('Pie')
+        registration_page.type_email('Cakes_and_bakery@mail.com')
+        registration_page.click_gender('male')
+        registration_page.type_number('9991234567')
+        registration_page.type_date_of_birth('23', 'August', '2023')
+        registration_page.type_subjects('Maths')
+        registration_page.click_hobbies('Music')
+        registration_page.download_picture(str(Path(__file__).parent.joinpath(f'image/Trixie.jpeg')))
+        registration_page.type_address('Sugar Palace, a candy store in Ponyville.')
+        registration_page.type_state('NCR')
+        registration_page.type_city('Noida')
+        registration_page.click_submit()
+
+    with allure.step("Check form results"):
+        registration_page.registered_user_data_should(
+            'Pinkamena Pie',
+            'Cakes_and_bakery@mail.com',
+            'Male',
+            '9991234567',
+            '23 August,2023',
+            'Maths',
+            'Music',
+            'Trixie.jpeg',
+            'Sugar Palace, a candy store in Ponyville.',
+            'NCR Noida'
+        )
