086919c68415364520e078455f8b6d49303c6ad9
Trixie_LM <octavia-94@bk.ru>
2023-11-07 20:00:29 +0300

    commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,34 @@
+"""фикстуры используемые для тестирования формы
+https://demoqa.com/automation-practice-form"""
 import pytest
-from selene.support.shared import browser
-from selene import have
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import browser
+from utils import attach
 
 
-@pytest.fixture(scope='function', autouse=True)
-def browser_management():
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub", options=options)
+    browser.config.base_url = "https://demoqa.com"
+    browser.config.driver = driver
+    browser.config.timeout = 2.0
 
-    # Browser settings
-    browser.config.browser_name = 'chrome'
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1900
-    browser.config.window_height = 1600
-    browser.config.timeout = 6.0
-    yield
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
-
-
-
